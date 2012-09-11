57b582a34d8d324be5e9aea2c5e32af216ea9308
Eric Moritz <eric@themoritzfamily.com>
2012-09-11 17:10:51 -0400

    Finished the initial stream parsing
    There is a bug that doesn't read the entire buffer if it's all MP3 data

--- src/streamsink_parser.erl
+++ src/streamsink_parser.erl
@@ -19,34 +19,34 @@ init() ->
     #parser_state{}.
 
 -spec parse(binary(), #parser_state{}) -> 
-    #parser_state{} | 
     {MP3Bytes :: binary(), MetaDataBytes :: binary(), #parser_state{}}.
 parse(Bytes, State = #parser_state{phase=reading_status, buffer=Buffer}) ->
     AllBytes = <<Buffer/binary, Bytes/binary>>,
-    case erlang:decode_packet(line, AllBytes, []) of
-        {ok, <<"ICY 200 OK\r\n">>, Rest} -> 
-            State#parser_state{phase=reading_headers, buffer=Rest};
-        {more, _Length} -> 
-            State#parser_state{buffer=AllBytes}
-    end;
+    NewState = case erlang:decode_packet(line, AllBytes, []) of
+                   {ok, <<"ICY 200 OK\r\n">>, Rest} -> 
+                       State#parser_state{phase=reading_headers, buffer=Rest};
+                   {more, _Length} -> 
+                       State#parser_state{buffer=AllBytes}
+               end,
+    {<<>>, <<>>, NewState};
 parse(Bytes, State = #parser_state{phase=reading_headers, buffer=Buffer, headers=Headers}) ->
     AllBytes = <<Buffer/binary, Bytes/binary>>,
-    case erlang:decode_packet(httph, AllBytes, []) of
-        {ok, http_eoh, Rest} -> 
-            % locate the Icy-Metaint value
-            ChuckSize = case proplists:get_value("Icy-Metaint", Headers) of
-                            undefined ->
-                                undefined;
-                            Value ->
-                                list_to_integer(Value)
-                        end,
-            State#parser_state{phase=reading_stream, mp3_chunksize=ChuckSize, buffer=Rest};
-        {ok, {http_header, _, Name, _, Value}, Rest} -> 
-            Header = {Name, Value},
-            State#parser_state{buffer=Rest, headers=[Header|Headers]};
-        {more, _Length} -> 
-            State#parser_state{buffer=AllBytes}
-    end;
+    NewState = case erlang:decode_packet(httph, AllBytes, []) of
+                   {ok, http_eoh, Rest} -> 
+                       ChuckSize = case proplists:get_value("Icy-Metaint", Headers) of
+                                       undefined ->
+                                           undefined;
+                                       Value ->
+                                           list_to_integer(Value)
+                                   end,
+                       State#parser_state{phase=reading_stream, mp3_chunksize=ChuckSize, buffer=Rest};
+                   {ok, {http_header, _, Name, _, Value}, Rest} -> 
+                       Header = {Name, Value},
+                       State#parser_state{buffer=Rest, headers=[Header|Headers]};
+                   {more, _Length} -> 
+                       State#parser_state{buffer=AllBytes}
+               end,
+    {<<>>, <<>>, NewState};
 parse(Bytes, State = #parser_state{phase=reading_stream,
                                    mp3_chunksize=undefined}) ->
     % If mp3_chunksize is undefined, that means all bytes in the stream
@@ -71,7 +71,7 @@ decode_stream(Bytes, ChunkSize, BytesRead)
   when size(Bytes) + BytesRead =< ChunkSize ->
     {Bytes, <<>>, <<>>, size(Bytes) + BytesRead};
 decode_stream(Bytes, ChunkSize, BytesRead) ->
-    MP3Size = BytesRead - ChunkSize,
+    MP3Size = ChunkSize - BytesRead,
     <<MP3Bytes:MP3Size/bytes, MetaBytes/binary>> = Bytes,
     case decode_meta(MetaBytes) of
         {MetaData, Rest} ->
@@ -117,49 +117,107 @@ decode_stream_test_() ->
 parse_test_() ->
     [
      % reading status
-     ?_assertEqual(#parser_state{phase=reading_headers, buffer= <<>>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_headers, buffer= <<>>}},
                    parse(<<"ICY 200 OK\r\n">>, #parser_state{})),
-     ?_assertEqual(#parser_state{phase=reading_headers, buffer= <<"icy-">>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_headers, buffer= <<"icy-">>}},
                    parse(<<"ICY 200 OK\r\nicy-">>, #parser_state{})),
-     ?_assertEqual(#parser_state{phase=reading_headers, buffer= <<>>},
+     ?_assertEqual({<<>>, <<>>,
+                   #parser_state{phase=reading_headers, buffer= <<>>}},
                    parse(<<" 200 OK\r\n">>, #parser_state{buffer= <<"ICY">>})),
 
      % reading headers
-     ?_assertEqual(#parser_state{phase=reading_headers,
-                                 buffer= <<"icy-metaint: 1">>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_headers,
+                                  buffer= <<"icy-metaint: 1">>}},
                    parse(<<"icy-metaint: 1">>,
                          #parser_state{phase=reading_headers})),
 
-     ?_assertEqual(#parser_state{phase=reading_headers,
-                                 buffer= <<"icy-metaint: 1\r\n">>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_headers,
+                                  buffer= <<"icy-metaint: 1\r\n">>}},
                    parse(<<"icy-metaint: 1\r\n">>,
                          #parser_state{phase=reading_headers})),
 
-     ?_assertEqual(#parser_state{phase=reading_headers,
-                                 headers=[{"Icy-Metaint", "1"}],
-                                 buffer= <<"\r\n">>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_headers,
+                                  headers=[{"Icy-Metaint", "1"}],
+                                  buffer= <<"\r\n">>}},
                    parse(<<"icy-metaint: 1\r\n\r\n">>,
                          #parser_state{phase=reading_headers})),
      
-     ?_assertEqual(#parser_state{phase=reading_stream, 
-                                 mp3_chunksize=undefined,
-                                 buffer= <<>>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_stream, 
+                                  mp3_chunksize=undefined,
+                                  buffer= <<>>}},
                    parse(<<"\r\n">>,
                          #parser_state{phase=reading_headers})),
 
-     ?_assertEqual(#parser_state{phase=reading_stream,
-                                 mp3_chunksize=8196,
-                                 headers=[{"Icy-Metaint", "8196"}],
-                                 buffer= <<>>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_stream,
+                                  mp3_chunksize=8196,
+                                  headers=[{"Icy-Metaint", "8196"}],
+                                  buffer= <<>>}},
                    parse(<<"\r\n">>,
                          #parser_state{phase=reading_headers,
                                        headers=[{"Icy-Metaint", "8196"}]})),
 
      
-     ?_assertEqual(#parser_state{phase=reading_stream, buffer= <<"a">>},
+     ?_assertEqual({<<>>, <<>>,
+                    #parser_state{phase=reading_stream, buffer= <<"a">>}},
                    parse(<<"\r\na">>,
-                         #parser_state{phase=reading_headers}))
+                         #parser_state{phase=reading_headers})),
 
+     % reading stream
+     % no metaint
+     ?_assertEqual({<<"abc">>, <<>>,
+                    #parser_state{phase=reading_stream}},
+                   parse(<<"abc">>,
+                         #parser_state{phase=reading_stream})),
+
+     % meta chunk not reached
+     ?_assertEqual({<<"abc">>, <<>>,
+                    #parser_state{phase=reading_stream,
+                                  mp3_chunkbytes=3,
+                                  mp3_chunksize=10}},
+                   parse(<<"abc">>,
+                         #parser_state{phase=reading_stream,
+                                      mp3_chunksize=10})),
+
+     % meta chunk reached but not full
+     ?_assertEqual({<<"abc">>, <<>>,
+                    #parser_state{phase=reading_stream,
+                                  buffer= <<1, "buf">>,
+                                  mp3_chunkbytes=10,
+                                  mp3_chunksize=10}},
+                   parse(<<"abc", 1, "buf">>,
+                         #parser_state{phase=reading_stream,
+                                       mp3_chunkbytes=7,
+                                       mp3_chunksize=10})),
+     % meta chunk complete
+     ?_assertEqual({<<"abc">>, <<"buf", 0:104>>,
+                    #parser_state{phase=reading_stream,
+                                  buffer= <<>>,
+                                  mp3_chunkbytes=0,
+                                  mp3_chunksize=10}},
+                   parse(<<"abc", 1, "buf", 0:104>>,
+                         #parser_state{phase=reading_stream,
+                                       mp3_chunkbytes=7,
+                                       mp3_chunksize=10})),
+
+     % meta chunk complete buffered
+     ?_assertEqual({<<>>, <<"buf", 0:104>>,
+                    #parser_state{phase=reading_stream,
+                                  buffer= <<>>,
+                                  mp3_chunkbytes=0,
+                                  mp3_chunksize=10}},
+                   parse(<<0:104>>,
+                         #parser_state{phase=reading_stream,
+                                       buffer= <<1, "buf">>,
+                                       mp3_chunkbytes=10,
+                                       mp3_chunksize=10}))
+     
      ].
     
 
